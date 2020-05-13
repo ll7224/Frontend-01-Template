07707cea324bddf4e1dbc1448eb9e9be502f713c
stoner <lqf898823@163.com>
2020-05-14 01:45:32 +0800

    作业提交

--- week05/toy-browser/client.js
+++ week05/toy-browser/client.js
@@ -31,6 +31,7 @@ ${this.bodyText}
     }
     send(connection) {
         return new Promise((resolve, reject) => {
+            const parser = new ResponseParser()
             if (connection) {
                 connection.write(this.toString())
             } else {
@@ -42,7 +43,13 @@ ${this.bodyText}
                 })
             }
             connection.on('data', (data) => {
-                resolve(data.toString());
+                parser.receive(data.toString())
+                // resolve(data.toString());
+                if (parser.isFinished) {
+                    resolve(parser.response)
+                }
+                console.log(parser.statusLine)
+                console.log(parser.headers)
                 connection.end()
             })
             connection.on('error', (err) => {
@@ -58,8 +65,158 @@ class Response {
 
 }
 
-class ResponseParse {
 
+//状态机
+class ResponseParser {
+    constructor() {
+        this.WAITING_STATUS_LINE = 0;
+        this.WAITING_STATUS_LINE_END = 1;
+        this.WAITING_HEADER_NAME = 2;
+        this.WAITING_HEADER_SPACE = 3;
+        this.WAITING_HEADER_VALUE = 4;
+        this.WAITING_HEADER_LINE_END = 5;
+        this.WAITING_HEADER_BLOCK_END = 6;
+        this.WAITING_BODY = 7;
+
+        this.current = this.WAITING_STATUS_LINE
+        this.statusLine = "";
+        this.headers = {}
+        this.headerName = ""
+        this.headerValue = ""
+        this.bodyParser = {}
+    }
+    get isFinished() {
+        return this.bodyParser && this.bodyParser.isFinished
+    }
+    get response() {
+        this.statusLine.match(/HTTP\/1.1 ([0-9]+) ([\s\S]+)/)
+        return {
+            statusCode: RegExp.$1,
+            statusText: RegExp.$2,
+            headers: this.headers,
+            body: this.bodyParser.content.join('')
+        }
+    }
+    receive(string) {
+        for (let i = 0; i < string.length; i++) {
+            this.receiveChar(string.charAt(i));
+        }
+    }
+    receiveChar(char) {
+        if (this.current === this.WAITING_STATUS_LINE) {
+            if (char === '\r') {
+                this.current = this.WAITING_STATUS_LINE_END
+            } else if (char === '\n') {
+                this.current = this.WAITING_HEADER_NAME
+            } else {
+                this.statusLine += char
+            }
+        }
+        //等待line_end
+        else if (this.current === this.WAITING_STATUS_LINE_END) {
+            if (char === '\n') {
+                this.current = this.WAITING_HEADER_NAME
+            }
+        }
+        //处理header_name
+        else if (this.current === this.WAITING_HEADER_NAME) {
+            if (char === ':') {
+                this.current = this.WAITING_HEADER_SPACE
+            } else if (char === '\r') {
+                this.current = this.WAITING_HEADER_BLOCK_END
+                if (this.headers['Transfer-Encoding'] === 'chunked') {
+                    this.bodyParser = new TrunkedBodyParser()
+                }
+            } else {
+                this.headerName += char
+            }
+        }
+        // 处理header_space
+        else if (this.current === this.WAITING_HEADER_SPACE) {
+            if (char === ' ') {
+                this.current = this.WAITING_HEADER_VALUE
+            }
+        }
+
+        // 处理header_value
+        else if (this.current === this.WAITING_HEADER_VALUE) {
+            if (char === '\r') {
+                this.current = this.WAITING_HEADER_LINE_END
+                this.headers[this.headerName] = this.headerValue
+                this.headerValue = ""
+                this.headerName = ""
+            } else {
+                this.headerValue += char
+            }
+        }
+
+        //
+        else if (this.current === this.WAITING_HEADER_LINE_END) {
+            if (char === '\n') {
+                //一行header_line_end结束，循环下一行header_name
+                this.current = this.WAITING_HEADER_NAME
+            }
+        }
+        //
+        else if (this.current === this.WAITING_HEADER_BLOCK_END) {
+            if (char === '\n') {
+                this.current = this.WAITING_BODY
+            }
+        }
+        //
+        else if (this.current === this.WAITING_BODY) {
+            this.bodyParser.receiveChar(char)
+        }
+    }
+}
+
+class TrunkedBodyParser {
+    constructor() {
+        this.WAITING_LENGTH = 0;
+        this.WAITING_LENGTH_LINE_END = 1;
+        this.READING_TRUNK = 2;
+        this.WAITING_NEW_LINE = 3;
+        this.WAITING_NEW_LINE_END = 4;
+        this.length = 0;
+        this.content = []
+        this.isFinished = false;
+        this.current = this.WAITING_LENGTH
+    }
+    receiveChar(char) {
+        if (this.current === this.WAITING_LENGTH) {
+            if (char === '\r') {
+                if (this.length === 0) {
+                    console.log(this.content)
+                    this.isFinished = true
+                }
+                this.current = this.WAITING_LENGTH_LINE_END
+            } else {
+                this.length *= 10
+                this.length += char.charCodeAt(0) - '0'.charCodeAt(0)
+            }
+        }
+        //
+        else if (this.current === this.WAITING_LENGTH_LINE_END) {
+            if (char === '\n') {
+                this.current = this.READING_TRUNK
+            }
+        } else if (this.current === this.READING_TRUNK) {
+            this.content.push(char)
+            this.length--;
+            if (this.length === 0) {
+                this.current = this.WAITING_NEW_LINE
+            }
+        } else if (this.current === this.WAITING_NEW_LINE) {
+            if (char === '\r') {
+                this.current = this.WAITING_NEW_LINE_END
+            }
+            //
+        } else if (this.current === this.WAITING_NEW_LINE_END) {
+            if (char === '\n') {
+                this.current = this.WAITING_LENGTH
+            }
+        }
+    }
 }
 
 void async function () {
--- week05/toy-browser/server.js
+++ week05/toy-browser/server.js
@@ -6,7 +6,7 @@ const server = http.createServer((req, res) => {
     res.setHeader('Content-Type', 'text/html')
     res.setHeader('X-Foo', 'bar')
     res.writeHead(200, {
-        'Content-Tyoe': 'text/html'
+        'Content-Type': 'text/html'
     });
     res.end('ok')
 })
